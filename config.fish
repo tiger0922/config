b686ef5dc94fcc75ff4724b133f83938c5cfbd44
Min-Han Tsai <tiger0922@gmail.com>
2024-07-29 11:29:25 +0800

    Modify fish config

--- config.fish
+++ config.fish
@@ -5,9 +5,9 @@ end
 starship init fish | source
 alias python=python3
 alias vim="nvim"
-alias ls="exa --icons"
-alias ll="exa -lbhF --git --icons"
-alias tree="exa --tree --icons"
+alias ls="eza --icons"
+alias ll="eza -lbhF --git --icons"
+alias tree="eza --tree --icons"
 alias cat="bat"
 alias zj="zellij"
 set -x FZF_DEFAULT_OPTS '--height 40%'
@@ -16,6 +16,3 @@ set -x REACT_EDITOR 'nvim'
 set -U EDITOR nvim
 set -x TERMINFO '/usr/share/terminfo'
 set -Ux MACOSX_DEPLOYMENT_TARGET 13.1
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/tsaiminhan/google-cloud-sdk/path.fish.inc' ]; . '/Users/tsaiminhan/google-cloud-sdk/path.fish.inc'; end
